72adeeb811096b9ff5e3447664519292c9cb1b3e
lulufa390 <1042767545@qq.com>
2018-10-12 14:40:04 -0700

    update data augmentation

--- classification/classifier.py
+++ classification/classifier.py
@@ -4,21 +4,27 @@ import torchvision.transforms as transforms
 from resnet import *
 
 
-transform=transforms.Compose([
+train_transform=transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     ])
 
-batch_size = 32
+test_transform=transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+
+
+batch_size = 64
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
+                                        download=True, transform=train_transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                           shuffle=True, num_workers=2)
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
+                                       download=True, transform=test_transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                          shuffle=False, num_workers=2)
 
@@ -104,7 +110,7 @@ def testing_accuracy(net, device, batch_size):
 
 
 
-for epoch in range(100):
+for epoch in range(50):
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         inputs, labels = data
