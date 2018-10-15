5ee206a9698cb3359f2640e34057ba6cc54ad937
jimmy <jimmy@westlake.cs.ubc.ca>
2018-10-15 16:35:17 -0700

    minor fix

--- classification/classifier.py
+++ classification/classifier.py
@@ -1,8 +1,19 @@
+import os
+import argparse
+
 import torch
 import torchvision
 import torchvision.transforms as transforms
 import torch.backends.cudnn as cudnn
+
 from resnet import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N', help='start epoch number')
+parser.add_argument('--resume', default='', metavar='PATH', help='path to the saved checkpoint')
+
+
+args = parser.parse_args()
 
 
 normalize = transforms.Normalize(mean=[0.491, 0.482, 0.447], std=[0.247, 0.243, 0.262])
@@ -46,6 +57,18 @@ if torch.cuda.is_available():
     net = nn.DataParallel(net).cuda()
 
 print(device)
+
+if args.resume:
+    if os.path.isfile(args.resume):
+        print('load checkpoint "{}"'.format(args.resume))
+        checkpoint = torch.load(args.resume)
+        net = checkpoint['state_dict']
+        epoch = checkpoint['epoch']
+        #optimizer = checkpoint['optimizer']
+
+        args.start_epoch = epoch
+    else:
+        print('file not found at "{}"'.format(args.resume))
 
 import torch.optim as optim
 criterion = nn.CrossEntropyLoss().cuda(device)
@@ -104,10 +127,14 @@ def adjust_learning_rate(optimizer, epoch):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
+def save_checkpoint(state, filename):
+    filepath = os.path.join(filename)
+    torch.save(state, filepath)
 
 
-for epoch in range(160):
+for epoch in range(args.start_epoch, 160):
     running_loss = 0.0
+    adjust_learning_rate(optimizer, epoch)
     for i, data in enumerate(trainloader, 0):
         inputs, labels = data
         inputs, labels = inputs.to(device), labels.to(device)
@@ -119,16 +146,23 @@ for epoch in range(160):
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
+    print('[Epoch: %d] training loss: %.5f' % (epoch + 1, running_loss / 50000))
+
 
     if epoch == 0 or epoch%10 == 9:
-        print('[Epoch: %d] loss: %.5f' % (epoch + 1, running_loss / 50000))
         training_accuracy(net, device, batch_size)
         testing_accuracy(net, device, 100)
 
-    adjust_learning_rate(optimizer, epoch)
+    if epoch%50 == 0:
+        save_name = str(epoch).zfill(4) + '_checkpoint.pth'
+        save_checkpoint({'epoch':epoch+1,
+                         'state_dict': net.state_dict(),
+                         'optimizer': optimizer.state_dict()},
+                        save_name)
+        print('save_name')
+
 
 print('Finished Training')
-
 
 testing_accuracy(net, device, 100)
 
